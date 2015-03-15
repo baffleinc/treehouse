93766b62cf6d108f2deaa2e4081f55ec7ac368f2
Harley Alexander <iharley@baffleinc.com>
2015-03-15 17:19:02 +1100

    woohoo ready to go

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewGrid'])
-	.controller('MainController', function ($scope, $window){
+angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewGrid', 'uiGmapgoogle-maps'])
+	.controller('MainController', function ($scope, $window, $log, Global, uiGmapGoogleMapApi){
+
+		$scope.global = Global;
 
 		if(!Modernizr.touch){
 			var windowHeight = $window.innerHeight;
@@ -9,23 +11,72 @@ angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewG
 
 			var logoTop = windowHeight + 140 + "px";
 
-			new ScrollMagic.Scene({
-				duration: windowHeight - 80
-			})
-			.setTween("#hero__logo", 0.5, { top: logoTop, width: "80px", marginLeft: "-40px", transform: "rotateY(0deg)" })
-			.addTo(smController);
+			new ScrollMagic.Scene({	duration: windowHeight - 80 })
+				.setTween("#hero__logo", 0.5, { top: logoTop, width: "80px", marginLeft: "-40px", transform: "rotateY(0deg)" })
+				.addTo(smController);
 
-			new ScrollMagic.Scene({
-				duration: windowHeight / 2
-			})
-			.setTween("#treehouse", 0.5, { opacity: 0, marginTop: "-100px" })
-			.addTo(smController);
+			new ScrollMagic.Scene({	duration: windowHeight / 2 })
+				.setTween("#treehouse", 0.5, { opacity: 0, marginTop: "-100px" })
+				.addTo(smController);
 
-			new ScrollMagic.Scene({
-				duration: windowHeight / 2
-			})
-			.setTween("#studios", 0.5, { marginTop: "-150px", opacity: 0 })
-			.addTo(smController);
+			new ScrollMagic.Scene({	duration: windowHeight / 2 })
+				.setTween("#studios", 0.5, { marginTop: "-150px", opacity: 0 })
+				.addTo(smController);
+
+			new ScrollMagic.Scene({	duration: windowHeight / 2 })
+				.setTween("#mouse", 0.5, { opacity: 0 })
+				.addTo(smController);
+
+		}
+
+		$scope.coords = {
+			sydney: { latitude: -33.8791282, longitude: 151.2157934 },
+			melbourne: { latitude: -37.8272442, longitude: 144.9933963 },
+		}
+
+	    var mapObj;
+
+		$scope.map = { 
+			center: $scope.coords.melbourne, 
+			zoom: 17,
+			events: {
+				tilesloaded: function(map){
+					mapObj = map;
+				}
+			}
+		};
+
+		$scope.switchMap = function(coords){
+			mapObj.setCenter(coords);
+		}
+
+		$scope.c = {
+			attempted: false,
+			loading: false,
+			disabled: false,
+			buttonText: "Drop that line"
+		};
+
+		$scope.submitContact = function(isValid){
+			if(isValid){
+				$scope.loading = true;
+				$scope.c.buttonText = '<i class="fa fa-circle-o-notch fa-spin"></i>';
+
+				$http({
+					method: 'POST',
+					url: window.AJAX.url,
+					params: {
+						nonce: window.AJAX.nonce,
+						contact: $scope.c
+					}
+				})
+				.success(function(resp){
+					$scope.c.buttonText = 'Thanks! Talk to you soon.';
+					$scope.c.disabled   = true;
+				})
+			} else {
+				$scope.attempted = true;
+			}
 		}
 
 
@@ -153,7 +204,7 @@ angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewG
 	];
 	})
 
-.directive('stickyHeader', function ($document, $window, Interface){
+.directive('stickyHeader', function ($document, $window, Interface, Global){
 	return {
 		restrict: 'AC',
 		link: function(scope, el, attrs){
@@ -166,6 +217,11 @@ angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewG
 				} else if (docScroll < elScroll) {
 					el.removeClass('stick');
 					Interface.logo.removeClass('hide');
+
+					if(Global.menuOpen){
+						Global.menuOpen = false;
+						scope.$apply();
+					}
 				}
 			});
 		}
@@ -188,4 +244,18 @@ angular.module('treehouse', ['hamburgerHelper', 'ngTouch', 'duScroll', 'previewG
 	};
 
 	return Interface;
+})
+
+.service('Global', function(){
+	var Global = {
+		menuOpen: false
+	};
+
+	return Global;
+})
+
+.filter('trust', function($sce){
+	return function(input){
+		return $sce.trustAsHtml(input);
+	}
 })
